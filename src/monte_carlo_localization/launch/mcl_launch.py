00c5a1d726e520e49c215733899e7fe11655131f
Myungsub <leemyungsub@hanyang.ac.kr>
2026-07-27 01:54:44 +0900

    맵 변경 (ifac_track -> map)

--- src/monte_carlo_localization/launch/mcl_launch.py
+++ src/monte_carlo_localization/launch/mcl_launch.py
@@ -36,7 +36,7 @@ def generate_launch_description():
     
     map_name_arg = DeclareLaunchArgument(
         'map_name',
-        default_value=os.environ.get('F1_MAP', 'map'),
+        default_value='map',
         description='Map name (without .yaml extension)'
     )
     
@@ -102,7 +102,9 @@ def generate_launch_description():
         'publish_map_odom_tf': PythonExpression([
             "'false' if '", LaunchConfiguration('mod'), "' == 'sim' else 'true'"
         ]),
-        'publish_odom_base_tf': False
+        'publish_odom_base_tf': PythonExpression([
+            "'false' if '", LaunchConfiguration('mod'), "' == 'sim' else 'true'"
+        ])
     }
     
     # === COMMON PARAMETERS ===
